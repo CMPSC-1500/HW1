8a9723d9b2b213cfe7352f5e0fc61e4afb41dd78
Dustin Monroe <dmonroe@nebrwesleyan.edu>
2018-02-21 21:06:37 -0600

    Upload program

---
+++ DMonroe/Homewrok1/src/Filesort.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Filesort {
+   public static void main (String[] args){
+       Scanner scan=new Scanner(System.in);
+
+       System.out.println("Enter Number of files");
+       int numOfFiles=Integer.parseInt(scan.nextLine());
+
+       String arrayOfFiles[]= new String[numOfFiles];
+        for (int i=0; i< arrayOfFiles.length; i++){
+            System.out.println("Enter the name of file " + (i+1) + ":");
+                arrayOfFiles[i]= scan.nextLine();
+        }
+       for (int i = 0; i < arrayOfFiles.length; i++) {
+             String phrase = arrayOfFiles[i];
+             String wBunRun = "BunRun";
+             String wZombie = "Zombie";
+             String wBoston = "Boston";
+             String wSpartan = "Spartan";
+             String wMud = "Mud";
+             //i figured out the if else statement without the &&! alex helped me come up with the if else to get only one key word//
+
+            if (phrase.contains(wBunRun)&&! phrase.contains(wBoston)&&!phrase.contains(wMud)&&!phrase.contains(wSpartan)&&!phrase.contains(wZombie))
+           {System.out.println(arrayOfFiles[i]);}
+            else if (phrase.contains(wZombie)&&! phrase.contains(wBoston)&&!phrase.contains(wMud)&&!phrase.contains(wSpartan)&&!phrase.contains(wBunRun))
+                {System.out.println(arrayOfFiles[i]);}
+                else if (phrase.contains(wBoston)&&! phrase.contains(wBunRun)&&!phrase.contains(wMud)&&!phrase.contains(wSpartan)&&!phrase.contains(wZombie))
+                     {System.out.println(arrayOfFiles[i]);}
+                     else if (phrase.contains(wSpartan)&&! phrase.contains(wBoston)&&!phrase.contains(wMud)&&!phrase.contains(wBunRun)&&!phrase.contains(wZombie))
+                          {System.out.println(arrayOfFiles[i]);}
+                          else if (phrase.contains(wMud)&&! phrase.contains(wBoston)&&!phrase.contains(wBunRun)&&!phrase.contains(wSpartan)&&!phrase.contains(wZombie))
+                               {System.out.println(arrayOfFiles[i]);}
+                            
+
+           }
+
+
+       }
+
+
+   }
+
